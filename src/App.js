04f1890f5ad011eff8c3abda3e79fa1ca8f84029
MaliMeghana <meghanamagare96@gmail.com>
2021-01-20 09:42:13 +0530

    19-01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-
-import './App.css';
-
-
+import "./App.css";
+import { Carousel } from "react-bootstrap";
 
 function App() {
-  let list=["Mum","del","kol","Che"];
+  let list = [
+    "https://picsum.photos/300/301",
+    "https://picsum.photos/300/302",
+    "https://picsum.photos/300/303",
+    "https://picsum.photos/300/304",
+    "https://picsum.photos/300/305",
+    "https://picsum.photos/300/306",
+    "https://picsum.photos/300/307",
+    "https://picsum.photos/300/308",
+  ];
+
   return (
-    <div className="App">
+    <div className="container-fluid">
       <div className="row">
-       <div className="d-none d-md-block col-md-3">COL1</div>
-       <div className="col-12 col-md-6">
-       {list.map((item,index)=>
-        <div className="d-flex justify-content-center align-items-center text-light  bg-info mb-2" style={{height:"150px",fontSize:"2rem"}}>
-        {item} </div>
-       )}
-       
-       </div>
-       <div className="d-none d-md-block col-md-3">COL1</div>
+        <div className="d-none d-md-block col-md-3 ">COL1</div>
+
+        {/** Can I Add Children Here */}
+        <div className="col-12 col-md-6 ">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/200/300"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis
+                  interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/200/301"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://picsum.photos/200/303"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+
+        <div className="d-none d-md-block col-md-3 ">COL3</div>
       </div>
     </div>
   );
