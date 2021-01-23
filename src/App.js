acf963475c00e892dcad3c9d935086ac16be852a
MaliMeghana <meghanamagare96@gmail.com>
2021-01-23 10:50:53 +0530

    some errors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,155 @@
+
+  
 import "./App.css";
-import { Carousel } from "react-bootstrap";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
 
 function App() {
-  return(
-       <div>
-         <Post/>
-      </div>
+  const location = useLocation();
+  console.log(location);
+
+  return (
+    <>
+      {!["/login", "/signup"].includes(location.pathname) && (
+        <>
+          <Link to="/">Page1 </Link>
+          <Link to="/page2">Page2 </Link>
+          <Link to="/page3">Page3 </Link>
+        </>
+      )}
+
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+
+      <Route exact path="/" component={Page1} />
+      <Route path="/page1" component={Page1} />
+      <Route path="/page2" component={Page2} />
+      <Route path="/page3" component={Page3} />
+    </>
   );
 }
-function Post(){
-  
-  return(
+
+function Page1() {
+  return <div>Page1</div>;
+}
+
+function Page2() {
+  const history = useHistory();
+
+  const signOut = () => {
+    history.push("/login");
+  };
+
+  return (
     <div>
-    
+      Page2
+      <div>
+        <button onClick={signOut}>Sign Out</button>
+      </div>
     </div>
   );
 }
-export default App;
 
+function Page3() {
+  return <div>Page3</div>;
+}
+
+function Login() {
+  const history = useHistory();
+
+  const processLogin = () => {
+    history.push("/");
+  };
+
+  const gotoRegister = () => {
+    history.push("/signup");
+  };
+
+  return (
+    <div>
+      <div>Login Page Ui</div>
+      <button onClick={processLogin}>Login</button>
+      <button onClick={gotoRegister}>Go To Regsiter</button>
+    </div>
+  );
+}
+
+function Signup() {
+  const history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+    email: "",
+    mobile: "",
+  });
+
+  const syncName = (e) => setUser({ ...user, name: e.target.value });
+  const syncPassword = (e) =>
+    setUser({ ...user, password: e.target.value });
+  const syncEmail = (e) => setUser({ ...user, email: e.target.value });
+  const syncMobile = (e) => setUser({ ...user, mobile: e.target.value });
+
+  const regiserUser = () => {
+    if (user.name === "" || user.password === "") {
+      return;
+    }
+
+    console.log("user", user);
+    localStorage.setItem("app-user", JSON.stringify(user));
+  };
+
+  const backtoLogin = () => {
+    history.push("/login");
+  };
+
+  return (
+    <div>
+      <h3>Sign Up Form</h3>
+
+      <div>
+        Name{" "}
+        <input
+          type="text"
+          value={user.name}
+          onChange={syncName}
+          required
+        />
+      </div>
+
+      <div>
+        Pswd{" "}
+        <input
+          type="password"
+          value={user.password}
+          onChange={syncPassword}
+          required
+        />
+      </div>
+
+      <div>
+        Email <input type="text" value={user.email} onChange={syncEmail} />
+      </div>
+
+      <div>
+        Mobile{" "}
+        <input type="text" value={user.mobile} onChange={syncMobile} />
+      </div>
+
+      <div>
+        <button onClick={regiserUser}>Register</button>
+      </div>
+
+      <div>
+        <button onClick={backtoLogin}>Back to Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
